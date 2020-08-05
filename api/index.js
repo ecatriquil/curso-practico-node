dde01d8f5372d13fa01990bc9295634c741d548c
Emanuel Catriquil <emacatriquil@gmail.com>
2020-08-05 03:30:28 -0300

    Microservicio horizontal de posts

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
+
 const errors = require('../network/errors');
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
+
 app.use(errors);
 
 app.listen(config.api.port, function () {
